fa8a5bc0fcde6e5ccfc28f6df899b431842149ac
Lapeikutis <kristupas.lapeika@gmail.com>
2019-12-16 17:32:39 +0200

    fixed Big photo sizing

--- src/index.js
+++ src/index.js
@@ -20,23 +20,32 @@ function displayLargeImage(target) {
   const author = document.createElement("p");
   const height = document.createElement("p");
   const width = document.createElement("p");
+  const dataContainer = document.createElement("div");
+  const photoContainer = document.createElement("div");
 
   author.innerHTML = `<b>Author</b> : ${target.author}`;
   height.innerHTML = `<b>Height</b> : ${target.height}px`;
   width.innerHTML = `<b>Width</b> : ${target.width}px`;
 
   img.setAttribute("src", target.download_url);
-  img.setAttribute("height", "79%");
+  photoContainer.setAttribute("class", "photoContainer");
+  dataContainer.setAttribute("class", "dataContainer");
 
-  bigPhoto.appendChild(author);
-  bigPhoto.appendChild(height);
-  bigPhoto.appendChild(width);
-  bigPhoto.appendChild(img);
+  bigPhoto.appendChild(dataContainer);
+  bigPhoto.appendChild(photoContainer);
+
+  dataContainer.appendChild(author);
+  dataContainer.appendChild(height);
+  dataContainer.appendChild(width);
+
+  photoContainer.appendChild(img);
 }
 
 smallPhotos.addEventListener("click", event => {
-  const id = event.target.id;
-  const photoInfo = photos.filter(elem => elem.id == id);
+  if (event.target.id == "smallPhotos") {
+    return;
+  }
+  const photoInfo = photos.filter(elem => elem.id == event.target.id);
 
   displayLargeImage(photoInfo[0]);
 });
